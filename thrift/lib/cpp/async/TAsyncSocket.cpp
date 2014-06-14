f4a84f83a33953a877416513657f02fb9e0fe398
Daniel Sloof <goapsychadelic@gmail.com>
2014-06-14 21:43:43 +0200

    [1/2] fix thrift build on OSX

--- thrift/lib/cpp/async/TAsyncSocket.cpp
+++ thrift/lib/cpp/async/TAsyncSocket.cpp
@@ -1088,6 +1088,7 @@ int TAsyncSocket::setQuickAck(bool quickack) {
 
   }
 
+#ifdef TCP_QUICKACK
   int value = quickack ? 1 : 0;
   if (setsockopt(fd_, IPPROTO_TCP, TCP_QUICKACK, &value, sizeof(value)) != 0) {
     int errnoCopy = errno;
@@ -1096,6 +1097,7 @@ int TAsyncSocket::setQuickAck(bool quickack) {
             << strerror(errnoCopy);
     return errnoCopy;
   }
+#endif
 
   return 0;
 }
@@ -1613,12 +1615,19 @@ ssize_t TAsyncSocket::performWrite(const iovec* vec,
   msg.msg_controllen = 0;
   msg.msg_flags = 0;
 
-  int msg_flags = MSG_DONTWAIT | MSG_NOSIGNAL;
+  int msg_flags = MSG_DONTWAIT;
+#if defined(MSG_NOSIGNAL)
+  msg_flags |= MSG_NOSIGNAL;
+#elif defined(SO_NOSIGPIPE)
+  msg_flags |= SO_NOSIGPIPE;
+#endif
   if (isSet(flags, WriteFlags::CORK)) {
     // MSG_MORE tells the kernel we have more data to send, so wait for us to
     // give it the rest of the data rather than immediately sending a partial
     // frame, even when TCP_NODELAY is enabled.
+#ifdef MSG_MORE
     msg_flags |= MSG_MORE;
+#endif
   }
   if (isSet(flags, WriteFlags::EOR)) {
     // marks that this is the last byte of a record (response)
