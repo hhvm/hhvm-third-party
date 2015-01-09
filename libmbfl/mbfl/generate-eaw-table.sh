d0cad57b5427f84af8f77d60b8b53de4aaea4694
JoelMarcey <joelm@fb.com>
2015-01-08 16:31:35 -0800

    De-clown my inadverdent hello echo
    Summary:
    
    It is always nice to see a random "Hello" when running `cmake`, right?
    
    sigh

--- libmbfl/mbfl/generate-eaw-table.sh
+++ libmbfl/mbfl/generate-eaw-table.sh
@@ -2,8 +2,7 @@
 awk_exec=$1
 source_dir=$2
 file="$source_dir/eaw_table.h"
-echo "Hello"
 if [ ! -f "$file" ]
 then
-  $awk_exec -v TABLE_NAME=mbfl_eaw_table -f $source_dir/mk_eaw_tbl.awk $source_dir/EastAsianWidth.txt > $source_dir/eaw_table.h
+  $awk_exec -v TABLE_NAME=mbfl_eaw_table -f $source_dir/mk_eaw_tbl.awk $source_dir/EastAsianWidth.txt > $file
 fi
