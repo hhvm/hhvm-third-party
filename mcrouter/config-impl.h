a6504d1863fe2f7346d6d80de02309682ab4277a
Sara Golemon <sgolemon@fb.com>
2015-02-12 15:08:27 -0800

    Fix mcrouter build
    * Generate ascii_client.c (avoid ragel dep)
    * Depend on folly
    * Compile C files (not just C++)
    * Skip examples

--- mcrouter/config-impl.h
+++ mcrouter/config-impl.h
@@ -1,1 +1,6 @@
-mcrouter/DestinationClient.h
+#ifndef incl_THIRD_PARTY_MCROUTER_CONFIG_IMPL_H
+#define incl_THIRD_PARTY_MCROUTER_CONFIG_INPL_H
+
+#include "mcrouter/DestinationClient.h"
+
+#endif // incl_THIRD_PARTY_MCROUTER_CONFIG_IMPL_H
--- mcrouter/config.h
+++ mcrouter/config.h
@@ -1,1 +1,15 @@
-mcrouter/mcrouter_config.h
+#ifndef incl_THIRD_PARTY_MCROUTER_CONFIG_H
+#define incl_THIRD_PARTY_MCROUTER_CONFIG_H
+
+#ifndef HAVE_CONFIG_H
+# define HAVE_CONFIG_H
+#endif
+#ifndef LIBMC_FBTRACE_DISABLE
+# define LIBMC_FBTRACE_DISABLE
+#endif
+
+#define MCROUTER_NO_MCROUTER_INSTANCE
+
+#include "mcrouter/mcrouter_config.h"
+
+#endif // incl_THIRD_PARTY_MCROUTER_CONFIG_H
