6ea7b8f9debf3912a36bf32839a75b85f504290d
JoelMarcey <joelm@fb.com>
2015-04-29 21:30:30 -0700

    DestinationClient was removed. Functionality put into ProxyDestination. See ff3186df32c8206c1d276b00219a9acb7ffdd272

--- mcrouter/config-impl.h
+++ mcrouter/config-impl.h
@@ -1,6 +1,6 @@
 #ifndef incl_THIRD_PARTY_MCROUTER_CONFIG_IMPL_H
 #define incl_THIRD_PARTY_MCROUTER_CONFIG_IMPL_H
 
-#include "mcrouter/DestinationClient.h"
+#include "mcrouter/ProxyDestination.h"
 
 #endif // incl_THIRD_PARTY_MCROUTER_CONFIG_IMPL_H
