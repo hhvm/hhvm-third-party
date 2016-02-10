64b89b9a6605f773356d10aae401c2aaeaf61375
Orvid King <blah38621@gmail.com>
2016-02-10 12:53:37 -0600

    Quote $awk_exec when generating the eaw table
    Because otherwise paths with spaces in the name, such as `C:\Program Files\`, will cause generation to fail.

--- libmbfl/mbfl/generate-eaw-table.sh
+++ libmbfl/mbfl/generate-eaw-table.sh
@@ -4,5 +4,5 @@ source_dir=$2
 file="$source_dir/eaw_table.h"
 if [ ! -f "$file" ]
 then
-  $awk_exec -v TABLE_NAME=mbfl_eaw_table -f $source_dir/mk_eaw_tbl.awk $source_dir/EastAsianWidth.txt > $file
+  "$awk_exec" -v TABLE_NAME=mbfl_eaw_table -f $source_dir/mk_eaw_tbl.awk $source_dir/EastAsianWidth.txt > $file
 fi
