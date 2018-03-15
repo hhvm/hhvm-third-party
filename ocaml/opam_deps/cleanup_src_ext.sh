52eae56e78893baf65ff1eef23df5c84037c76b1
Vassil Mladenov <vmladenov@fb.com>
2018-03-15 15:17:04 -0700

    Added headers to shell scripts

--- ocaml/opam_deps/cleanup_src_ext.sh
+++ ocaml/opam_deps/cleanup_src_ext.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#
+# Copyright (c) 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
 set -e
 SRC_EXT=$1
 # opam does not gitignore .download but does gitignore .stamp
